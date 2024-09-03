b29cfc2c7f786715f50d4a6ff4d9b766990e3465
mehrad <mmfirst@protonmail.com>
2024-09-03 18:31:11 +0330

    small refactor

--- src/lib/UrlCard.svelte
+++ src/lib/UrlCard.svelte
@@ -1,12 +1,14 @@
 <script>
     export let shortUrl, url;
+    import { page } from "$app/stores";
+
     const copyShort = () => {
-        navigator.clipboard.writeText(shortUrl);
+        navigator.clipboard.writeText(`${$page.url.origin}/${shortUrl}`);
     };
 </script>
 
 <div class="card px-5 py-2 grid has-text-centered">
     <a class="cell" href={url}>{url.slice(0, 25) + "..."}</a>
-    <a class="cell" href={shortUrl}>{shortUrl.split("/")[3]}</a>
+    <a class="cell" href={shortUrl} bind>{shortUrl}</a>
     <button class="cell" on:click={copyShort}>Copy</button>
 </div>
